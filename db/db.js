0aba0a465e94c2aefcb6da2cfca21e1b73aab6b8
Jose Rodriguez <rdrgz.jose123@gmail.com>
2022-12-31 12:33:14 +0100

    fix: Modificando naming variables de entorno

--- db/db.js
+++ db/db.js
@@ -3,12 +3,12 @@ const {Sequelize} = require("sequelize")
 require("dotenv").config()
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
     {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: process.env.MYSQLPORT,
         dialect: process.env.DB_DIALECT
     }
 )
