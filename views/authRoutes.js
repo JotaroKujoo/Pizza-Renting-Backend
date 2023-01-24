b52eb57fd1a0d62341af592455b21abdf40064e6
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-01-24 15:22:46 +0100

    Recolocando bearer middleware

--- views/authRoutes.js
+++ views/authRoutes.js
@@ -9,6 +9,6 @@ const {
 
 router.post('/login', authLoginController)
 router.post('/register', authRegisterController)
-router.post("registeradmin", authRegisterControllerAdmin)
+router.post("/registeradmin", authRegisterControllerAdmin)
 
 module.exports = router;
