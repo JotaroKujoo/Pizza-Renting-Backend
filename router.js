9cb2b7ea3ee90cd3de5d8ee509c2248effd4df17
Jose Rodriguez <rdrgz.jose123@gmail.com>
2023-01-25 17:10:21 +0100

    fix: Modificando controlador de delete

--- router.js
+++ router.js
@@ -5,18 +5,18 @@ const authRoutes = require("./views/authRoutes")
 const pizzasRoutes = require("./views/pizzasRoutes")
 const userRoutes = require("./views/userRoutes")
 const pizzeriaRoutes = require("./views/pizzeriaRoutes")
+const {authBearerMiddleware} = require("./middlewares/authMiddleware")
 const ordersRoutes = require("./views/orderRoutes")
 const ingredientRoutes = require("./views/ingredientRoutes")
-const {authBearerMiddleware} = require("./middlewares/authMiddleware")
 
 
 router.use("/pizzeria",pizzeriaRoutes)
 router.use("/auth", authRoutes)
 router.use("/pizzas", pizzasRoutes)
 router.use("/ingredients",ingredientRoutes)
+router.use(authBearerMiddleware)
 router.use("/user", userRoutes)
 router.use("/orders", ordersRoutes)
-router.use(authBearerMiddleware)
 
 
 module.exports = router
